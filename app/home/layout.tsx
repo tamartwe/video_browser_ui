6307eaf26fa8c5dbad777e426fe3e3dfcb3ac5d6
xm <tamar.twena@xmcyber.com>
2025-01-04 23:31:59 +0200

    bug fix

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/Header';
-import axios from 'axios';
 
 export default function Layout({children, }: { children: React.ReactNode}) {
     return (
---
+++ app/model/vidoe.ts
@@ -0,0 +1,8 @@
+export default interface Video {
+    id: number;
+    artist: string;
+    title: string;
+    release_year: number;
+    genre_id: number;
+    image_url: string;
+}
